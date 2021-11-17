487318e39022b674c9e3ea09b1a8ad2bfa3c3413
fbarrientos <fbarrientos@uabc.edu.mx>
2021-11-16 18:08:47 -0800

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pasteleria_dd.Startup))]
+namespace pasteleria_dd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
