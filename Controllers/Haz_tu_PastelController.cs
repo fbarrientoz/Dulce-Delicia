f9f11d053f76233ff9df9b2d2be5b650b998018d
fbarrientos <fbarrientos@uabc.edu.mx>
2021-11-19 13:41:15 -0800

    Cambios en step

---
+++ Controllers/Haz_tu_PastelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pasteleria_dd.Controllers
+{
+    public class Haz_tu_PastelController : Controller
+    {
+        // GET: Haz_tu_Pastel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
