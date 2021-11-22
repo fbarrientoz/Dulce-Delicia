bda068712ca30a3ba33e7defc561700cab8502da
fbarrientos <fbarrientos@uabc.edu.mx>
2021-11-21 23:18:02 -0800

    Se agrego funcionalidad de haz tu pastel  y admin

--- Controllers/Haz_tu_PastelController.cs
+++ Controllers/Haz_tu_PastelController.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using pasteleria_dd.Models;
 
 namespace pasteleria_dd.Controllers
 {
     public class Haz_tu_PastelController : Controller
     {
+        private STNBEntities db = new STNBEntities();
         // GET: Haz_tu_Pastel
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,Tamanio,Sabor,Relleno,Foto,nombre,telefono,Datos_extra,correo_electronico")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedidos.Add(pedido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pedido);
         }
     }
 }
---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using pasteleria_dd.Models;
+
+namespace pasteleria_dd.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private STNBEntities db = new STNBEntities();
+
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            return View(db.Pedidos.ToList());
+        }
+
+        // GET: Pedido/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedido/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pedido/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Tamanio,Sabor,Relleno,Foto,estatus,nombre,telefono,direccion,fecha_solicitud,fecha_entrega,Datos_extra,correo_electronico")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pedidos.Add(pedido);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedido/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedido/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Tamanio,Sabor,Relleno,Foto,estatus,nombre,telefono,direccion,fecha_solicitud,fecha_entrega,Datos_extra,correo_electronico")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pedido).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedido/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pedido pedido = db.Pedidos.Find(id);
+            if (pedido == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pedido pedido = db.Pedidos.Find(id);
+            db.Pedidos.Remove(pedido);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
